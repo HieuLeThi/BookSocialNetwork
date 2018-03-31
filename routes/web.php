aed7da9ef6265fdebb93346901df2cbc2e6e4147
HieuLeThi <hieule95it@gmail.com>
2018-03-31 11:11:14 +0700

    Add route test GUI

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+	return view('frontend.home.index');
+});
+Route::get('/user', function () {
+	return view('frontend.user.index');
+});
+Route::get('mybook', function () {
+	return view('frontend.user.mybook');
+});
+Route::get('bookdetail', function () {
+	return view('frontend.home.detailbook');
+});
